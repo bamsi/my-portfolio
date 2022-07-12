e039c82abd1ceba5ab62fe0e73679fd3df9f2300
bamsi <hibamsi@gmail.com>
2022-07-12 18:31:05 +0300

    create hamburger menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      My portfolio
-    </div>
+    <main className="main-container">
+      <div className="header">
+        <Header />
+        <Navbar />
+      </div>
+    </main>
   );
 }
 
